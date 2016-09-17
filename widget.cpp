fe6c14e6b16d3f4fb3e2bf5171bef3b4ffd88373
xf <xf@ax.DHCP>
2016-09-18 01:13:34 +0800

    trying to merge acc and gyro to IMU.W

--- widget.cpp
+++ widget.cpp
@@ -9,6 +9,7 @@
 #include <QMessageBox>
 #include <QHostAddress>
 #include <QtWidgets/QBoxLayout>
+#include <QTime>
 
 #define X_POINT_SIZE 200
 
@@ -37,6 +38,8 @@ Widget::Widget(QWidget *parent) :
 
 
 
+
+
 }
 
 void Widget::init_plot()
@@ -49,7 +52,7 @@ void Widget::init_plot()
     // axes names
     plot->setAxisScale( QwtPlot::xBottom, 0, X_POINT_SIZE);
     plot->setAxisTitle(QwtPlot::xBottom, "t (s)" );
-    plot->setAxisScale( QwtPlot::yLeft, -1.5, 1.5 );
+    plot->setAxisScale( QwtPlot::yLeft, -2.0, 2.0 );
     plot->setAxisTitle(QwtPlot::yLeft, "y (g)");
 
     plot->setLineWidth(1);
@@ -78,6 +81,7 @@ Widget::~Widget()
 void Widget::connectedSlot()
 {
     isConnected = true;
+    g_timer.start();
 }
 
 void Widget::disconnectedSlot()
@@ -88,15 +92,34 @@ void Widget::disconnectedSlot()
 
 void Widget::readyReadSlot()
 {
+    double _gx, _gy, _gz;
     //QByteArray message;
     //message = socket->readAll();
-    socket->read((char *)&sd, sizeof(sd));
+    socket->read((char *)&sd_raw, sizeof(sd));
 
-    //memset(&sd, (int)&message, sizeof(sd));
-    qDebug("read ax %d, ay %d, az %d, gx %d, gy %d, gz %d\r\n",
-           sd.ax, sd.ay, sd.az, sd.gx, sd.gy, sd.gz);
+    sd.ax = -sd_raw.ax;
+    sd.ay = -sd_raw.ay;
+    sd.az = -sd_raw.az;
+    //qDebug() << "ax " << sd.ax << ", ay " << sd.ay << ", az " << sd.az;
+    a_u = sqrt(sd.ax*sd.ax + sd.ay *sd.ay + sd.az*sd.az);
 
-    addpoint((double)sd.ax / 12768, (double)sd.ay / 12768, (double)sd.az / 12768);
+   // arc cos (x ,y z), the unit is rad.
+    xra.xr_ax = acos(sd.ax / a_u);
+    xra.xr_ay = acos(sd.ay / a_u);
+    xra.xr_az = acos(sd.az / a_u);
+    qDebug() << "xra.xr_ax " << xra.xr_ax << ", xra.xr_ay " << xra.xr_ay << ", xra.xr_az " << xra.xr_az;
+
+
+    _gx = (double)sd.gx / 131;
+    _gy = (double)sd.gy / 131;
+    _gz = (double)sd.gz / 131;
+
+    int timer_diff = g_timer.elapsed();
+    g_timer.start();
+
+    //qDebug() << "timer elapse = %d" << timer_diff;
+
+    addpoint((double)sd.ax / 16384, (double)sd.ay / 16384, (double)sd.az / 16384);
 
 }
 
--- widget.h
+++ widget.h
@@ -9,10 +9,15 @@
 #include <qwt_plot.h>
 #include <qwt_plot_canvas.h>
 #include <qwt_plot_curve.h>
+#include <QTime>
 
 struct sensor_data {
     int16_t ax, ay, az;
     int16_t gx, gy, gz;
+};
+
+struct xr {
+    double xr_ax, xr_ay, xr_az;
 };
 
 
@@ -54,11 +59,15 @@ private:
     Ui::Widget *ui;
     QTcpSocket *socket;
     bool isConnected;
+    struct sensor_data sd_raw;
     struct sensor_data sd;
+    double a_u; // acce uniform value
+    struct xr xra;
 
     QwtPlot *plot;
     QwtPlotCurve *curve_ax, *curve_ay, *curve_az;
     QVector<double> *ax, *ay, *az, *at;
+    QTime g_timer;
 
     void init_plot();
 };
